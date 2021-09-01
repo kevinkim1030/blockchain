78f66c303c9c0dff206c95a7534a023bdbddbd72
kevinkim1030 <kevin.kim1030@gmail.com>
2021-09-01 15:07:52 -0400

    created basic endpoints

--- dev/api.js
+++ dev/api.js
@@ -1,27 +1,42 @@
 const express = require('express');
 const app = express();
 const Blockchain = require('./blockchain');
+const { v4: uuidv4 } = require('uuid');
+
+const nodeAddress = uuidv4().split('-').join('');
 
 const firecoin = new Blockchain();
 
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json());
 
-app.get('/', function (req, res) {
-  res.send('HELLO WORLD FROM ROOT VIA EXPRESS')
-})
-
 app.get('/blockchain', function (req, res) {
-  
+  res.send(firecoin);
 });
 
-app.post('/transactions', function (req, res) {
-  console.log(req.body);
-  res.send(`The amount of the transaction is ${req.body.amount} firecoin`);
+app.post('/transaction', function (req, res) {
+  const blockIndex = firecoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
+  res.json({ note: `Transaction will be added in block ${blockIndex}`})
 });
 
 app.get('/mine', function (req, res) {
+  const lastBlock = firecoin.getLastBlock;
+  const previousBlockHash = lastBlock['hash'];
+  const currentBlockData = {
+    transactions: firecoin.pendingTransactions,
+    index: lastBlock['index'] + 1
+  }
+  const nonce = firecoin.proofOfWork(previousBlockHash, currentBlockData);
+  const blockHash = firecoin.hashBlock(previousBlockHash, currentBlockData, nonce);
 
+  // mining reward
+  firecoin.createNewTransaction(6.25, "00", nodeAddress);
+
+  const newBlock = firecoin.createNewBlock(nonce, previousBlockHash, blockHash);
+  res.json({
+    note: "New block mined succesfully",
+    block: newBlock
+  })
 });
 
 app.listen(3000, () => {
